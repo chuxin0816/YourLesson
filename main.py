637695f3ff1533bb636be61f8a986bc143d4da7c
chuxin <im.chuxin@gmail.com>
2024-08-30 22:54:11 +0800

    better

--- main.py
+++ main.py
@@ -10,29 +10,38 @@ import choose_course
 if __name__ == "__main__":
 
     for i in range(setting.count):
+        cnt = 0
         for course in setting.courses:
             try:
-                response = choose_course.start_choose(
-                    course['id'], course['type'])
-
-                time.sleep(setting.delay/1000.0)
-                
-                # if "该课程超过课容量" in response:
-                #     print("该课程超过课容量")
-                #     break
-                # elif "添加选课志愿成功" in response:
-                #     print("抢课成功")
-                #     break
-                # else:
-                #     print(course['name']+": "+response)
+                response = choose_course.start_choose(course['id'], course['type'])
                 if "添加选课志愿成功" in response:
-                    print(course['name'],": 抢课成功")
-        
+                    print("抢课成功")
+                    cnt += 1
+                elif "非法请求" in response:
+                    print(response)
+                    sys.exit()
+                elif "已经存在选课结果中" in response:
+                    cnt += 1
+                elif "该课程超过课容量" in response:
+                    print(course['name'] + ": " + response)
+                elif "选课系统正在初始化" in response:
+                    print(response)
+                    time.sleep(100)
+                else:
+                    print(response)
+                    sys.exit()
+
             except KeyboardInterrupt:
                 print("通过键盘中断退出程序")
                 sys.exit()
             except:
                 print("出现错误，请检查设置setting.py部分是否填写正确")
+                print(i + 1)
+                sys.exit()
+        if cnt >= len(setting.courses):
+            break
+
+        time.sleep(setting.delay / 1000.0)
 
     print("抢课结束")
 
--- setting.py
+++ setting.py
@@ -13,10 +13,11 @@
 
 user_id = "2022150091"
 
-cookie = "_WEU=T705A2ZMlmBePuySlyHQQYUo7W9pvr2gmOQe48iwq4NS73qNezsNRIptWb*zwg1D; JSESSIONID=F5B6B297FCD1170DDEC9093426C33BBD; _webvpn_key=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMjAyMjE1MDA5MSIsImdyb3VwcyI6WzE1XSwiaWF0IjoxNjk0MzU3MzQ4LCJleHAiOjE2OTQ0NDM3NDh9.H45NeDjltqHKlVBPr6uD-rpDBezfOnFVm4WrH-RynQs; webvpn_username=2022150091%7C1694357348%7Caac656bc6b5f9d1148c269a6ff81f0614b3a4619; insert_cookie=33651787; iPlanetDirectoryPro=3SVxZaH5xCG26vdXYgBMM0; nZUAHEcHJdbMO=60klwG25fxLZsoNwSJhHxXVpzGAz0Iy.1lxdkweF.hwY.aRCoCw.SctwHoNR0aIKs8RFMD18RC.J3ST2d1C9il_q"
-electiveBatchCode = "f0af76e1e27f4e9e9a7c4f495aa04655"
+# cookie = ""
+cookie = "_WEU=qggsnUO1E7TZLQE4IGPyofwDF7aAx9XKcCtNm35LMCBqyAFzzc_Q6HYMmB78hrjx; JSESSIONID=266BC0ABBF5B3A161870DFFE521FFC2D; route=2e7b4a35a31c5a6d82a1048539c55f40; insert_cookie=22322123"
+electiveBatchCode = "3e05cb5370a2428f991fa902fe078b66"
 
-token = "29a26510-9fbb-4858-816b-e65771728245"
+token = "63824583-6d06-4528-8c16-4cf3bda64226"
 
 # 本班课程： 'TJKC'
 # 方案内课程: 'FANKC'
@@ -27,16 +28,19 @@ token = "29a26510-9fbb-4858-816b-e65771728245"
 # 体育课程:"TYKC"
 # 你要抢的课程，按照如下格式提前先填写好
 courses = [
-    {'id': '202320241150291000101', 'type': 'FANKC', 'name': "计算机安全导论"},
-    {'id': '202320241150061000302', 'type': 'FANKC', 'name': "计算机图形学"},
-    {'id': '202320241510003000101', 'type': 'FANKC', 'name': "军事理论"},
+    # {'id': '202420251150291000101', 'type': 'TJKC', 'name': "计算机安全导论"},
+    # {'id': '202420251150289000101', 'type': 'TJKC', 'name': "微处理器与机器人"},
+    # {'id': '202420251150280000101', 'type': 'TJKC', 'name': "自动机与形式语言"},
+    # {'id': '202420251150280000101', 'type': 'FANKC', 'name': "自动机与形式语言"},
+    {'id': '202420251150329000101', 'type': 'FANKC', 'name': "智能识别系统设计"},
+    {'id': '202420251150465000101', 'type': 'FANKC', 'name': "计算机视觉"},
 ]
 
 # 抢课的间隔，单位是毫秒
-delay:int = 350
+delay:int = 60*1000
 
 # 抢课的次数
-count:int = 1000
+count:int = 10*60*60*1000//delay
 
 
 
